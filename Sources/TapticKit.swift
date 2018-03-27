a7304e3f492efb32b0f1d2d045509a7abcce9d2f
Andrey Sevrikov <devandsev@gmail.com>
2018-03-27 20:37:56 +0300

    Add Xcode project

---
+++ Sources/TapticKit.swift
@@ -0,0 +1,9 @@
+//
+//  TapticKit.swift
+//  Andrey Sevrikov
+//
+//  Created by Andrey Sevrikov on 27/03/2018.
+//  Copyright © 2018 Andrey Sevrikov. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/TapticKitTests/TapticKitTests.swift
@@ -0,0 +1,23 @@
+//
+//  TapticKitTests.swift
+//  Andrey Sevrikov
+//
+//  Created by Andrey Sevrikov on 27/03/2018.
+//  Copyright © 2018 Andrey Sevrikov. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import TapticKit
+
+class TapticKitTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(TapticKit().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
